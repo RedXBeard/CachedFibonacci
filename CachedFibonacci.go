c1d100ec40389b49e800e87744dd6073a031926e
Barbaros Yıldırım <barbarosaliyildirim@gmail.com>
2016-03-09 23:23:48 +0200

    iterative number can be taken, if input is 0 then sys exits

--- CachedFibonacci.go
+++ CachedFibonacci.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/patrickmn/go-cache"
+	"os"
 	"strconv"
 	"time"
 )
@@ -11,7 +12,10 @@ func fibonacci(s uint64, c *cache.Cache) uint64 {
 	cached, found := c.Get(strconv.FormatUint(s, 32))
 	if found {
 		return cached.(uint64)
-	} else if s < 2 {
+	} else if s == 0 {
+		c.Set(strconv.FormatUint(s, 32), uint64(0), cache.DefaultExpiration)
+		return 0
+	} else if s < 3 {
 		c.Set(strconv.FormatUint(s, 32), uint64(1), cache.DefaultExpiration)
 		return 1
 	} else {
@@ -25,7 +29,13 @@ func main() {
 	c := cache.New(5*time.Minute, 30*time.Second)
 
 	var input uint64
-	fmt.Print("Number : ")
-	fmt.Scanln(&input)
-	fmt.Println(fibonacci(input, c))
+	for true {
+		fmt.Print("Number : ")
+		fmt.Scanln(&input)
+		if input == 0 {
+			os.Exit(1)
+		} else {
+			fmt.Println(fibonacci(input, c))
+		}
+	}
 }
