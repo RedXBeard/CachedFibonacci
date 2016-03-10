8e9372fcac0ffcc8be7bba114b8b1370be78c037
Barbaros Yıldırım <barbaros.yildirim@markafoni.com>
2016-03-10 08:30:13 +0200

    factorial func added

--- CachedFibonacci.go
+++ CachedFibonacci.go
@@ -25,17 +25,56 @@ func fibonacci(s uint64, c *cache.Cache) uint64 {
 	}
 }
 
+func factorial(s uint64, c *cache.Cache) uint64 {
+	cached, found := c.Get(strconv.FormatUint(s, 32))
+	if found {
+		return cached.(uint64)
+	} else if s < 1 {
+		c.Set(strconv.FormatUint(s, 32), uint64(1), cache.DefaultExpiration)
+		return 1
+	} else {
+		result := s * factorial(s-1, c)
+		c.Set(strconv.FormatUint(s, 32), result, cache.DefaultExpiration)
+		return result
+	}
+}
+
+func intInSlice(a int, list []int) bool {
+    for _, b := range list {
+        if b == a {
+            return true
+        }
+    }
+    return false
+}
+
 func main() {
 	c := cache.New(5*time.Minute, 30*time.Second)
 
+	var funcType int
 	var input uint64
+	choiceSet := []int{0, 1}
+
+	fmt.Println("Please select your function call from the following")
+	fmt.Println("(0) Fibonacci Numbers")
+	fmt.Println("(1) Factorial of Number\n")
+	for true {
+		fmt.Print("Your Choice: ")
+		fmt.Scanln(&funcType)
+		if intInSlice(funcType, choiceSet) {
+			break
+		}
+	}
+
 	for true {
 		fmt.Print("Number : ")
 		fmt.Scanln(&input)
 		if input == 0 {
 			os.Exit(1)
-		} else {
+		} else if funcType == 0 {
 			fmt.Println(fibonacci(input, c))
+		} else if funcType == 1 {
+			fmt.Println(factorial(input, c))
 		}
 	}
 }
